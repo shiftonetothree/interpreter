c52162824dc640e4a86f189f0069a7ff6a49354f
张卓诚 <zhangzhuocheng@gagogroup.com>
2019-05-04 13:51:31 +0800

    feat(src/part-2): 添加第二趴

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { part1 } from "./part-1";
+import { part2 } from "./part-2";
 console.log(part1("1+2"));
 console.log(part1("3+4"));
+console.log(part2("1+2"));
+console.log(part2("3+4"));
+console.log(part2("12+23"));
+console.log(part2("3-4"));
+console.log(part2("12-23"));
