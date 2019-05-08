6ac500c8cc49406466076c99516295e21aa4cb6f
JamesZhang <shiftonetothree@126.com>
2019-05-08 08:02:33 +0800

    feat(part-3): 完成第3章

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { part1 } from "./part-1";
 import { part2 } from "./part-2";
+import { part3 } from "./part-3";
 console.log(part1("1+2"));
 console.log(part1("3+4"));
 console.log(part2("1+2"));
-console.log(part2("3+4"));
-console.log(part2("12+23"));
-console.log(part2("3-4"));
-console.log(part2("12-23"));
+console.log(part2("3 +4"));
+console.log(part2("12+ 23"));
+console.log(part2("3- 4"));
+console.log(part2("12 -23"));
+console.log(part3("1+2"));
+console.log(part3("3 +4"));
+console.log(part3("12+ 23"));
+console.log(part3("3- 4"));
+console.log(part3("12 -23"));
+console.log(part3("3-4-6 +8 -11"));
+console.log(part3("12-23 +10-20 +22"));
+try{
+    console.log(part3("12-23 +10-20 +22+"));
+}catch(e){
+    console.log("passed");
+}
