c32f8c4dc95d2e8de1fbcd4d88ab690d201e9cf8
JamesZhang <shiftonetothree@126.com>
2019-07-25 23:39:07 +0800

    feat(src/part-7): 完成第7章

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import { part6 } from "./part-6";
-console.log(part6("7 + 3 * (10 / (12 / (3 + 1) - 1))"));
+import { part7 } from "./part-7";
+console.log(part7("7 + 3 * (10 / (12 / (3 + 1) - 1))"));
